0fd6251a6a7cca341e172bbc9c64923d86559d45
Sandip Shrestha <124436727+sandipshrestha1423@users.noreply.github.com>
2025-11-11 22:36:11 +0545

    went straight to beggining

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,21 +1,24 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import logo from './logo.svg';
 import './App.css';
-import Login from './Component/Login';
-import Register from './Component/Register';
-import Home from './Component/Home';
-import Dashboard from './Component/Dashboard';
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-      </Routes>
-    </Router>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
